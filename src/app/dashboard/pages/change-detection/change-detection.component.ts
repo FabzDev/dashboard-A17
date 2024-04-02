71528b7ff3e01e474a5d29cf4ef2ed62bac8f2ee
Fabio <fabioescobarardila@hotmail.com>
2024-04-02 14:14:54 -0500

    Change Detection Completed

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'dashboard-change-detection',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TitleComponent],
   templateUrl: './change-detection.component.html',
-  styles: ``
+  styles: ``,
 })
 export default class ChangeDetectionComponent {
+  public frameworkAsProperty = { framework: 'Angular', release: '2016' };
+  public frameworkAsSignal = signal({ framework: 'Angular', release: 2016 });
+  public frameworkAsTitle = computed( () => `Framework: ${this.frameworkAsSignal().framework}`)
+  constructor(){
+    setTimeout(() => {
 
+      console.log('done');
+      this.frameworkAsSignal.update( value => ({...value, framework:'SolidJs'}))
+
+    }, 3000);
+  }
 }

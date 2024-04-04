2aca6a64bce09a36b4891553f511edf3be71ea52
Fabio <fabioescobarardila@hotmail.com>
2024-04-03 23:35:25 -0500

    implementing users view

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,15 +1,19 @@
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { UserService } from '@services/user.service';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'dashboard-user-list',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent, RouterLink],
   templateUrl: './users.component.html',
   styles: ``
 })
 export default class UsersComponent {
 
   public userService = inject(UserService)
+
+  public state = this.userService.state();
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,7 @@
-import { Injectable, signal } from '@angular/core';
-import { User } from '../interfaces/users-response.interface';
+import { computed, Injectable, signal, inject } from '@angular/core';
+import { User, UsersResponse } from '../interfaces/users-response.interface';
+import { HttpClient } from '@angular/common/http';
+import { delay } from 'rxjs';
 
 interface State {
   users: User[];
@@ -11,12 +13,20 @@ interface State {
 })
 export class UserService {
 
+  public http = inject(HttpClient)
+
   #state = signal<State>({
     users:[],
      loading: true
     })
 
+    public state = computed( () => this.#state() )
+
   constructor() {
-    console.log('Cargando Users');
+    this.http.get<UsersResponse>('https://reqres.in/api/users/')
+    .pipe(
+      delay(2000)
+    )
+    .subscribe( (usersResponse) => this.#state().users = usersResponse.data )
    }
 }

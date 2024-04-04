8ccb85b643375220482c8c3dff6a83e45cb33c5b
Fabio <fabioescobarardila@hotmail.com>
2024-04-04 12:10:34 -0500

    implementing single User view (toSignal Method)

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -4,23 +4,25 @@ import { ActivatedRoute } from '@angular/router';
 import { User } from '@interfaces/users-response.interface';
 import { UserService } from '@services/user.service';
 import { TitleComponent } from '@shared/title/title.component';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'dashboard-user-page',
   standalone: true,
   imports: [TitleComponent],
   template: `
-
     <section>
-      @if(currentUser()){
-      <shared-title [rootTitle]="'User: '+ userName()" ></shared-title>
-      <img [srcset]="currentUser()?.avatar"
-           [alt]="currentUser()?.first_name">
+      @if(signalCurrentUser()){
+      <shared-title [rootTitle]="'User: ' + userName()"></shared-title>
+      <img [srcset]="signalCurrentUser()?.avatar" [alt]="signalCurrentUser()?.first_name" />
 
-      <span>{{currentUser()!.first_name}} {{currentUser()!.last_name}}</span>
-      <p>{{currentUser()!.email}}</p>\
+      <span
+        >{{ signalCurrentUser()!.first_name }} {{ signalCurrentUser()!.last_name }}</span
+      >
+      <p>{{ signalCurrentUser()!.email }}</p>
       } @else {
-        <h2>Cargando información del usuario...</h2>
+      <h2>Cargando información del usuario...</h2>
       }
     </section>
   `,
@@ -32,14 +34,32 @@ export default class UserComponent {
 
   //PROPERTIES
   public currentUser = signal<User | undefined>(undefined);
-  public userName = computed( () => `${this.currentUser()?.first_name} ${this.currentUser()?.last_name}` ?? '')
+  public userName = computed(
+    () =>
+      `${this.signalCurrentUser()?.first_name} ${this.signalCurrentUser()?.last_name}` ?? ''
+  );
+  public signalCurrentUser;
 
   //METHODS
+  // constructor() {
+
+  //   const id$ = this.routeParams.params.pipe(map( ({id}) => id ))
+
   constructor() {
-    this.routeParams.params.subscribe(({ id }) =>
-      this.userService
-        .getUserById(id)
-        .subscribe((user) => this.currentUser.set(user))
-    );
+    let userId = '';
+
+    this.routeParams.params.subscribe(({ id }) => userId = id)
+
+    this.signalCurrentUser = toSignal(
+      this.userService.getUserById( userId  )
+    )
+
   }
+  // constructor() {
+  //   this.routeParams.params.subscribe(({ id }) =>
+  //     this.userService
+  //       .getUserById(id)
+  //       .subscribe((user) => this.currentUser.set(user))
+  //   );
+  // }
 }
